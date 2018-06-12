b1e58656f78e1ae4d362562e900085632ec9cf4e
Gang Wang <gang.wang@broadcom.com>
2018-06-12 16:05:57 +0800

    Start predict

--- main.cpp
+++ main.cpp
@@ -22,8 +22,11 @@ int main() {
 //    //print result
 //    std::cout << "Xsig = " << std::endl << Xsig << std::endl;
 
-    MatrixXd Xsig_aug = MatrixXd(7, 15);
-    ukf.AugmentedSigmaPoints(&Xsig_aug);
+//    MatrixXd Xsig_aug = MatrixXd(7, 15);
+//    ukf.AugmentedSigmaPoints(&Xsig_aug);
+
+    MatrixXd Xsig_pred = MatrixXd(15, 5);
+    ukf.SigmaPointPrediction(&Xsig_pred);
 
     return 0;
 }
